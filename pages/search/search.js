5f04e40bde317482afb7c9e9c8de4549c8ee7f28
Alvin <huangyuanlei@hotmail.com>
2020-03-10 16:21:20 +0800

    Add search and setting pages

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,10 +1,39 @@
 var pageObject = {
   data: {
+    searchRes1: {
+      "singer": {
+        "count": 2,
+        "itemlist": [
+          {
+            "docid": "944940",
+            "id": "944940",
+            "mid": "0020PeOh4ZaCw1",
+            "name": "裘东 (Julian Qiu)",
+            "pic": "http://imgcache.qq.com/music/photo/mid_singer_58/w/1/0020PeOh4ZaCw1.jpg",
+            "singer": "裘东 (Julian Qiu)"
+          },
+          {
+            "docid": "89698",
+            "id": "89698",
+            "mid": "003Cn3Yh16q1MO",
+            "name": "邱明月",
+            "pic": "http://imgcache.qq.com/music/photo/mid_singer_58/M/O/003Cn3Yh16q1MO.jpg",
+            "singer": "邱明月"
+          }
+        ],
+        "name": "未打卡员工",
+        "order": 1,
+        "type": 2
+      },
 
+    }
   },
-  search: function(e) {
+  search: function (e) {
     var that = this
     console.log('搜索')
+    wx.showLoading({
+      title: "正在检索",
+    });
     wx.request({
       url: 'http://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg',
       data: {
@@ -25,15 +54,21 @@ var pageObject = {
       header: {
         'Content-Type': 'text/html;charset=utf-8'
       },
-      success: function(res) {
+      success: function (res) {
         // var pattern =new RegExp("\\((.*?)\\)","igm");
         that.setData({
-          searchRes: JSON.parse(res.data.substring(38, res.data.length - 1)).data
-        })
+          //searchRes: JSON.parse(res.data.substring(38, res.data.length - 1)).data
+        });
+        setTimeout( function(){
+          wx.hideLoading();
+          that.setData({
+            searchRes: that.data.searchRes1,
+          })
+        }, 2000);
       }
     })
   },
-  play: function(event) {
+  play: function (event) {
     var num = event.currentTarget.dataset.num
     var res = this.data.searchRes.song.itemlist[num]
     var appInstance = getApp()
@@ -48,11 +83,20 @@ var pageObject = {
       title: res.name,
       singer: res.singer,
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003RMaRI1iFoYd.jpg',
-      complete: function(res) {
+      complete: function (res) {
 
       }
     })
-  }
+  },
+  detail: function (event) {
+    var num = event.currentTarget.dataset.num;
+    var res = this.data.searchRes.singer.itemlist[num];
+    wx.showModal({
+      title: "咦",
+      content: "没有 " + res.name + " 的信息",
+      showCancel: false,
+    });
+  },
 }
 
 Page(pageObject)
--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,4 +1,15 @@
-
 Page({
-  
+  data: {
+    userInfo: {
+      avatarUrl: "https://pic2.zhimg.com/v2-c15aefb8fa14eada3f9ab102a54b6dca_1200x500.jpg",
+      nickName: "想太多",
+    },
+  },
+
+  save: function(event){
+    wx.showModal({
+      content: "已提交审核，请勿重复注册",
+      showCancel: false,
+    });
+  },
 });
